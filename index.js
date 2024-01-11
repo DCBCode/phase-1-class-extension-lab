bb98ba56ca73b3592377dd6459780f9d580e6695
Drew Benson <Drewbenson16@gmail.com>
2024-01-11 11:33:32 -0700

    not sure what i am doing wrong

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-// Your code here
+class Polygon {
+    constructor(sides) {
+      this.sides = sides;
+    }
+  
+    get countSides() {
+      return this.sides.length;
+    }
+  
+    get perimeter() {
+      return this.sides.reduce((sum, side) => sum + side, 0);
+    }
+  }
+  
+  class Triangle extends Polygon {
+    constructor(sides) {
+      super(sides);
+    }
+  
+    isValidTriangle() {
+      if (this.countSides !== 3) {
+        return false;
+      }
+  
+      const [sideA, sideB, sideC] = this.sides;
+      return (
+        sideA + sideB > sideC &&
+        sideA + sideC > sideB &&
+        sideB + sideC > sideA
+      );
+    }
+  }
+  
+  class Square extends Polygon {
+    constructor(sideLength) {
+      super([sideLength, sideLength, sideLength, sideLength]);
+      this.sideLength = sideLength;
+    }
+  
+    get area() {
+      return this.sideLength ** 2;
+    }
+  
+    isValidSquare() {
+      const [sideA, sideB, sideC, sideD] = this.sides;
+      return sideA === sideB && sideB === sideC && sideC === sideD;
+    }
+  }
+  
+  // Test case for Triangle class
+  const triangle = new Triangle([3, 4, 5]);
+  console.log(triangle.isValidTriangle()); // true
+  
+  // Test cases for Square class
+  const square = new Square(5);
+  console.log(square.perimeter); // 20
+  console.log(square.area); // 25
+  console.log(square.isValidSquare()); // true
